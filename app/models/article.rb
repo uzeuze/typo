c712ef315d6cba61e7ad711f023bd7a68427a81e
Egemen Can Uze <egemencanuze@gmail.com>
2016-02-03 19:19:09 +0000

    Add merge article

--- app/models/article.rb
+++ app/models/article.rb
@@ -415,6 +415,22 @@ class Article < Content
   def access_by?(user)
     user.admin? || user_id == user.id
   end
+  
+  def merge_with(other_article_id)
+    tomerge = Article.find_by_id(other_article_id)
+    if not self.id or not tomerge.id
+      return false
+    end
+
+    self.body = self.body + "\n\n" + tomerge.body
+    self.comments << tomerge.comments
+    self.save!
+
+    tomerge = Article.find_by_id(other_article_id)
+    tomerge.destroy
+
+    return true
+  end
 
   protected
 
